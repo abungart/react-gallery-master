93f153f9da93b1146fddcd96d4265c8099093e4f
Adam Bungart <abungart@gmail.com>
2020-04-12 19:14:53 -0500

    Base goals get put and conditional rendering complete

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 
 class GalleryItem extends Component {
+  state = {
+    clicked: false,
+  };
+
   render() {
     console.log("GalleryItem", this.props.item.path);
-    // let clicked = false;
+
     let galleryOut = (
-      <div className="gallery-container">
+      <div
+        className="gallery-container"
+        onClick={() => {
+          this.setState({ clicked: true });
+          console.log("in Clicked", this.state.clicked);
+        }}
+      >
         <img
           src={this.props.item.path}
           className="img-container"
@@ -16,16 +26,21 @@ class GalleryItem extends Component {
       </div>
     );
 
-    // if (clicked === true) {
-    //   galleryOut = (
-    //     <div className="gallery-container">
-    //       <p>{this.props.item.description}</p>
-    //       <button onClick={this.props.updateLike(this.props.item.id)}>
-    //         Like
-    //       </button>
-    //     </div>
-    //   );
-    // }
+    if (this.state.clicked === true) {
+      galleryOut = (
+        <div
+          className="gallery-container"
+          onClick={() => {
+            this.setState({ clicked: false });
+          }}
+        >
+          <p>{this.props.item.description}</p>
+          <button onClick={this.props.updateLike(this.props.item.id)}>
+            Like
+          </button>
+        </div>
+      );
+    }
 
     return galleryOut;
   }
